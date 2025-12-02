ef11caa45ae8360ab299f4ead5d49bd577e8465e
Aliaksandr Valchetski <alex.volchetsky@gmail.com>
2025-12-02 23:25:03 +0100

    use custom social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,8 +71,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.jpg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
